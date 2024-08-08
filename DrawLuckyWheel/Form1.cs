df1c72ee61e12564eef34dee4b53518202d7f436
niCholai <flyingkatar@gmail.com>
2024-08-08 13:12:49 +0700

    update

---
+++ DrawLuckyWheel/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLuckyWheel
+{
+    public partial class Form1 : Form
+    {
+        bool wheelIsMoved;    
+        float wheelTimes;      
+        Timer wheelTimer;
+        LuckyCirlce koloFortuny;
+        public Form1()
+        {
+            InitializeComponent();         
+            wheelTimer = new Timer();
+            wheelTimer.Interval = 30; // speed 
+            wheelTimer.Tick += wheelTimer_Tick;
+            koloFortuny = new LuckyCirlce();
+           
+        }
+        public class LuckyCirlce
+        {
+            public Bitmap obrazek;
+            public Bitmap tempObrazek;
+            public float kat;
+            public int[] wartosciStanu;
+            public int stan;
+
+            public LuckyCirlce()
+            {
+                tempObrazek = new Bitmap(Properties.Resources.lucky_wheel);
+                obrazek = new Bitmap(Properties.Resources.lucky_wheel);
+                wartosciStanu = new int[] { 12, 11, 10 ,09, 08, 07, 06, 05, 04, 03, 02, 01};
+                kat = 0.0f;
+            }
+
+        }
+
+      
+    }
+}
